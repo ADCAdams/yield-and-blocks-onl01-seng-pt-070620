240a62bc47edb165b323aeb5a89ed97b74cfe2ed
Andy Adams <andrewdcadams@gmail.com>
2020-07-09 15:12:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-def hello_t(array)
+def hello_t(array)        #builds a manual 'each' method
   if block_given?
     i = 0
    
@@ -15,7 +15,7 @@ end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|    #calls the each method and is able to enumerate through
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
